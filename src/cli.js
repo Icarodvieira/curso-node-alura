b1e1ff278ccf10cec7c6700b9877b2d5cb948f52
Icaro Vieira <icarodvieira3@gmail.com>
2024-09-02 22:04:44 -0300

    primeira biblioteca

---
+++ src/cli.js
@@ -0,0 +1,62 @@
+import fs from 'fs';
+import path from 'path';
+import trataErros from './erros/funcoesErro.js';
+import { contaPalavras } from './index.js'
+import { montaSaidaArquivo } from './helpers.js';
+import { Command } from 'commander';
+import chalk from 'chalk';
+
+const erro = chalk.bold.red;
+const sucesso = chalk.bold.green;
+
+
+const program = new Command ();
+
+program
+    .version('0.0.1')
+    .option('-t, --texto <string>', 'caminho do texto a ser processado')
+    .option('-d, --destino <string>', 'caminho da pasta onde será salvo os resultados')
+    .action((options) => {
+        const { texto, destino } = options;
+        if (!texto || !destino){
+            console.error(erro('erro: favor inserir caminho de origem ou destino'))
+            program.help();
+            return;
+        }
+
+        const caminhoTexto = path.resolve(texto);
+        const caminhoDestino = path.resolve(destino);
+        console.log(sucesso('Texto processado.'));
+        
+        try {
+            processaArquivo(caminhoTexto, caminhoDestino);
+        } catch (erro) {
+            console.log(erro('ocorreu um erro no processamento '), erro);
+            
+        }
+    })
+program.parse()
+
+function processaArquivo(texto,destino){
+    fs.readFile(texto, 'utf-8', (erro, texto)=>{
+        try{
+            if(erro) throw erro
+            const resultado = contaPalavras(texto);
+            criaESalvaArquivo(resultado, destino)
+        }catch (erro){
+            trataErros(erro);
+        }
+    })
+}
+
+async function criaESalvaArquivo(listaPalavras, endereco){
+    const arquivoNovo = `${endereco}/resultado.txt`;
+    const textoPalavras = montaSaidaArquivo(listaPalavras);
+    console.log(textoPalavras);
+    try{
+        await fs.promises.writeFile(arquivoNovo, textoPalavras);
+        console.log(sucesso('Arquivo Criado.'));
+    }catch(erro){
+        throw erro;
+    }
+}
